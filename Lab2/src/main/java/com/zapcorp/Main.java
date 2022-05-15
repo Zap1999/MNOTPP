9b47be4a8b1be361f543e9cfb04d845aeb1638f6
Zap1999 <avashchenock@gmail.com>
2022-05-15 14:02:33 +0300

    [Lab2] function tuned

--- Lab2/src/main/java/com/zapcorp/Main.java
+++ Lab2/src/main/java/com/zapcorp/Main.java
@@ -1,6 +1,5 @@
 package com.zapcorp;
 
-import com.zapcorp.util.MathUtil;
 import com.zapcorp.util.RandUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,15 +10,15 @@ public class Main {
 
     private static final int LIST_SIZE = 500_000;
     private static final int ELEMENTS_ORIGIN = 2;
-    private static final int ELEMENTS_BOUND = 10_000;
+    private static final int ELEMENTS_BOUND = 10_000_000;
 
-    private static final Function<Integer, Double> FUNCTION =
-            (x) -> MathUtil.sinh(MathUtil.factorial(x)) / MathUtil.cosh(MathUtil.factorial(-(2 * x)));
+    private static final Function<Integer, Double> FUNCTION = (x) -> Math.sqrt(123 * Math.pow(x, -Math.sqrt(x)) / 68) + x / 3;
 
 
     public static void main(final String... args) {
         final var list = RandUtil.generateList(LIST_SIZE, ELEMENTS_ORIGIN, ELEMENTS_BOUND);
 
 
+        log.info("");
     }
 }
