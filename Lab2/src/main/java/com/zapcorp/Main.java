7467e756b1e20fb9dee905ca84b0b45730f11071
Zap1999 <avashchenock@gmail.com>
2022-05-15 13:03:47 +0300

    [Lab2] function implemented

--- Lab2/src/main/java/com/zapcorp/Main.java
+++ Lab2/src/main/java/com/zapcorp/Main.java
@@ -1,6 +1,11 @@
 package com.zapcorp;
 
+import java.util.function.Function;
+
 public class Main {
+
+    private static final Function<Integer, Double> FUNCTION =
+            (x) -> MathUtil.sinh(MathUtil.factorial(x)) / MathUtil.cosh(MathUtil.factorial(-(2 * x)));
 
     public static void main(final String... args) {
 
